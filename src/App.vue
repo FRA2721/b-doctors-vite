8e712f75257f22554867bb8125d15bffff1e4085
FRA2721 <cimino.francesco36@gmail.com>
2023-02-10 13:14:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!-- script section -->
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    // name
+    name: "App",
+    components: {
+    AppHeader,
+  }
+}
+</script>
+
+
+
+<!-- template section -->
+<template>
+
+  <!-- header -->
+  <header>
+    <AppHeader />
+  </header>
+
+</template>
+
+
+
+
+<!-- style section -->
+<style lang="scss">
+@use './styles/general.scss' as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,102 @@
+<!-- script section -->
+<script>
+export default{
+  // name
+  name: "AppHeader",
+
+  // data
+  data(){
+    return{
+      links: [
+
+        // home
+        {
+          voice: "HOME",
+          href: "#home"
+        },
+
+        // about
+        {
+          voice: "ABOUT",
+          href: "#about"
+        },
+
+        // services
+        {
+          voice: "SERVICES",
+          href: "#services"
+        },
+
+        // contact
+        {
+          voice: "CONTACT",
+          href: "#contact"
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+
+
+
+<!-- template section -->
+<template>
+
+  <!-- header section -->
+  <header>
+    <nav>
+      <ul>
+        <li v-for="( item, index ) in links">
+          <a :href="item.href">{{ item.voice }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <!-- /header section -->
+
+</template>
+
+
+
+
+<!-- style section -->
+<style lang="scss" scoped>
+@use './styles/general.scss' as *;
+
+header{
+  background-image: linear-gradient(to bottom right, lightblue, rgb(216, 236, 243));
+
+    nav{
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 80%;
+
+    // list formatting section
+    ul{
+      display: flex;
+
+      // list items
+      li{
+        padding: 1rem;
+        border-top: 3px solid transparent;
+        transition-duration: .5s;
+
+        &:hover{
+          opacity: 1;
+          border-top: 3px solid white;
+        }
+
+        // anchor
+        a{
+          color: white;
+        }
+      }
+    }
+  }
+}
+
+</style>
