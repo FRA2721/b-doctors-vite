65969803236eba53c918f2dacfc12288283cce0f
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-02-13 00:10:07 +0100

    Add doctors "in evidence"

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,17 +1,13 @@
 <!-- script section -->
 <script>
 export default {
-    // name
-    name: "AppJumbotron"
-}
+  // name
+  name: "AppJumbotron",
+};
 </script>
 
-
-
-
-
 <template>
-    <!-- <section class="d-flex jumbotron-section">
+  <!-- <section class="d-flex jumbotron-section">
         <div class="jumbotron">
             <div class="jumbotron-container">
                 <h3>We care about you...</h3>
@@ -26,72 +22,68 @@ export default {
         </div>
     </section> -->
 
-    <section class="jumbotron">
-        <div class="jumbotron-container">
-            <div class="container">
-                <div class="row">
-                    <div
-                        class="jumbo-left pt-4 pb-4 col-12 col-lg-8 text-white text-center text-lg-start d-flex flex-column justify-content-center">
-                        <h3>We care about you...</h3>
-                        <h1 class="mb-3">THE BEST<br>ONLINE<br>SUPPORT FOR YOUR HEALTH</h1>
+  <section class="jumbotron">
+    <div class="jumbotron-container">
+      <div class="container">
+        <div class="row">
+          <div
+            class="jumbo-left pt-4 pb-4 col-12 col-lg-8 text-white text-center text-lg-start d-flex flex-column justify-content-center">
+            <h3>We care about you...</h3>
+            <h1 class="mb-3">
+              THE BEST<br />ONLINE<br />SUPPORT FOR YOUR HEALTH
+            </h1>
 
-                        <div class="call-to-action row g-2">
-                            <div class=" col-12 col-md-6 col-lg-3 col-xl-12">
-                                <a href="#" class="btn btn-primary">READ MORE</a>
-                            </div>
-                            <div
-                                class="col-12 col-md-6 col-lg-9 col-xl-12 d-flex justify-content-center justify-content-lg-start align-items-center">
-                                <div class="d-inline-block">
-                                    <a href="#" class="play-btn me-2"><i class="fa-regular fa-circle-play me-1"></i>
-                                    </a>
-                                </div>
-                                <div class="d-inline-block">Meet our
-                                    doctors</div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="jumbo-right d-none d-lg-block col-4">
-                        <div class="jumbo-img">
-                            <img src="../assets/imgs/jumbo-doctor.png" alt="">
-                        </div>
-                    </div>
+            <div class="call-to-action row g-2">
+              <div class="col-12 col-md-6 col-lg-3 col-xl-12">
+                <a href="#" class="btn btn-primary">READ MORE</a>
+              </div>
+              <div
+                class="col-12 col-md-6 col-lg-9 col-xl-12 d-flex justify-content-center justify-content-lg-start align-items-center">
+                <div class="d-inline-block">
+                  <a href="#" class="play-btn me-2"
+                    ><i class="fa-regular fa-circle-play me-1"></i>
+                  </a>
                 </div>
+                <div class="d-inline-block">Meet our doctors</div>
+              </div>
             </div>
+          </div>
+          <div class="jumbo-right d-none d-lg-block col-4">
+            <div class="jumbo-img">
+              <img src="../assets/imgs/jumbo-doctor.png" alt="" />
+            </div>
+          </div>
         </div>
-    </section>
-
-
+      </div>
+    </div>
+  </section>
 </template>
-
-
-
 
 <!-- style section -->
 <style lang="scss" scoped>
-@use '../styles/general.scss' as *;
+@use "../styles/general.scss" as *;
 
 .jumbotron {
-    background-color: #c3dee7;
+  background-image: linear-gradient(to bottom right, #2b94f1, #21ce99);
 
-    h3 {
-        font-size: 1.4rem;
-    }
+  h3 {
+    font-size: 1.4rem;
+  }
 
-    h1 {
-        font-size: 1.8rem;
-    }
+  h1 {
+    font-size: 1.8rem;
+  }
 
-    .jumbo-img {
-        img {
-            width: 100%;
-        }
+  .jumbo-img {
+    img {
+      width: 100%;
     }
+  }
 
-    .play-btn {
-        i {
-            font-size: 1.7rem;
-        }
+  .play-btn {
+    i {
+      font-size: 1.7rem;
     }
+  }
 }
 </style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,61 +1,125 @@
 <!-- script section -->
 <script>
-import { store } from '../store';
-import { RouterLink } from 'vue-router';
-import AppJumbotron from '../components/AppJumbotron.vue';
+import { store } from "../store";
+import { RouterLink } from "vue-router";
+import AppJumbotron from "../components/AppJumbotron.vue";
 
 export default {
-
-    name: 'AppHome',
-    components: { AppJumbotron, RouterLink },
-    data() {
-        return {
-            store,
-            label: "Search",
-            routeName: "doctorslist"
-
-
-        }
-    },
-
-}
+  name: "AppHome",
+  components: { AppJumbotron, RouterLink },
+  data() {
+    return {
+      store,
+      label: "Search",
+      routeName: "doctorslist",
+    };
+  },
+};
 </script>
 <!-- /script section -->
 
 <!-- template section -->
 <template>
-    <!-- jumbotron section -->
-    <div>
-        <AppJumbotron />
+  <!-- jumbotron section -->
+  <div>
+    <AppJumbotron />
+  </div>
+  <main>
+    <div class="bg-light mt-4">
+      <div class="container evidenza">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+            <div class="card" style="width: 22rem">
+              <img
+                src="../assets/imgs/doc-1.png"
+                class="card-img-top"
+                alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card h-100 d-inline-block" style="width: 22rem">
+              <img
+                src="../assets/imgs/doc-5.png"
+                class="card-img-top"
+                alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 22rem">
+              <img
+                src="../assets/imgs/doc-3.jpg"
+                class="card-img-top"
+                alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card h-100 d-inline-block" style="width: 22rem">
+              <img
+                src="../assets/imgs/doc-6.png"
+                class="card-img-top"
+                alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <main>
-
-        <div class="bg-success">
-            <div class="container evidenza">
-                medici in evidenza
-            </div>
-        </div>
-        <div class="container pt-3">
-            <div class="searchbar">
-                <input type="text" placeholder="Find a Doctor" v-model="this.store.searchKey">
-                <router-link :to="{ name: this.routeName }">{{ this.label }}</router-link>
-            </div>
-        </div>
-
-    </main>
+    <div class="container pt-3">
+      <div class="searchbar">
+        <input
+          type="text"
+          placeholder="Find a Doctor"
+          v-model="this.store.searchKey" />
+        <router-link :to="{ name: this.routeName }">{{
+          this.label
+        }}</router-link>
+      </div>
+    </div>
+  </main>
 </template>
 <!-- /template section -->
-
-
 
 <!-- style section -->
 <style lang="scss">
 // .jumbotron {
 //     min-height: 500px;
 // }
+.card-img-top {
+  width: 600px;
+}
 
 .evidenza {
-    min-height: 200px;
+  min-height: 200px;
 }
 </style>
 <!-- /style section -->
