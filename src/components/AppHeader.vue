a800f721394232fa37deffe69902c8a6efa925d6
luishs0 <luishs.doc@gmail.com>
2023-02-12 14:39:52 +0100

    .

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -58,9 +58,8 @@ export default {
                     <!-- /logo -->
 
                     <!-- hambunger menu -->
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                        aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <!-- /hamburger menu -->
