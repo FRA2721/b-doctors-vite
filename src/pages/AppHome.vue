230c2e73e09e3cad8589da43bc3e6d38d141cf7a
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-02-15 14:05:10 +0100

    add imgs

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -6,85 +6,105 @@ import { RouterLink } from "vue-router";
 import AppJumbotron from "../components/AppJumbotron.vue";
 
 export default {
-    name: "AppHome",
-    components: { AppJumbotron, RouterLink },
-    data() {
-        return {
-            store,
-            label: "Search",
-            routeName: "doctorslist",
-            baseUrl: 'http://127.0.0.1:8000',
-            doctors: [],
-        };
-    },
+  name: "AppHome",
+  components: { AppJumbotron, RouterLink },
+  data() {
+    return {
+      store,
+      label: "Search",
+      routeName: "doctorslist",
+      baseUrl: "http://127.0.0.1:8000",
+      doctors: [],
+    };
+  },
 
-    created() {
-        this.getDoctors();
-    },
+  created() {
+    this.getDoctors();
+  },
 
-    methods: {
-        getDoctors() {
-            axios.get(`${this.baseUrl}/api/profiles`, {
-                params: {
-                    ...this.store.docSearch && { specialization_id: this.store.docSearch }
-                }
-            }).then(resp => {
-                this.doctors = resp.data.results.user;
-                this.store.specializations = resp.data.results.specializations;
-                console.log(this.doctors);
-                this.loading = false;
-                if(this.store.docSearch){
-                    this.$router.push({name: 'doctorslist'})
-                }
-            })
-        }
+  methods: {
+    getDoctors() {
+      axios
+        .get(`${this.baseUrl}/api/profiles`, {
+          params: {
+            ...(this.store.docSearch && {
+              specialization_id: this.store.docSearch,
+            }),
+          },
+        })
+        .then((resp) => {
+          this.doctors = resp.data.results.user;
+          this.store.specializations = resp.data.results.specializations;
+          console.log(this.doctors);
+          this.loading = false;
+          if (this.store.docSearch) {
+            this.$router.push({ name: "doctorslist" });
+          }
+        });
     },
+  },
 };
 </script>
 <!-- /script section -->
 
 <!-- template section -->
 <template>
-    <!-- jumbotron section -->
-    <div>
-        <AppJumbotron />
-    </div>
-    <main>
-        <div class="container pt-3">
+  <!-- jumbotron section -->
+  <div>
+    <AppJumbotron />
+  </div>
+  <main>
+    <div class="container pt-3">
+      <div class="searchbar col-4 m-auto mt-3">
+        <select
+          class="form-select"
+          @change="getDoctors"
+          v-model="store.docSearch"
+          placeholder="test">
+          <option value="">choose a specialization</option>
+          <option
+            v-for="spec in store.specializations"
+            :value="spec.id"
+            :key="spec.id">
+            {{ spec.title }}
+          </option>
+        </select>
+      </div>
 
-            <div class="searchbar col-4 m-auto mt-3">
-                <select class="form-select" @change="getDoctors" v-model="store.docSearch" placeholder="test">
-                    <option value="">choose a specialization</option>
-                    <option v-for=" spec in store.specializations" :value="spec.id"> {{ spec.title }}</option>
-                </select>
-            </div>
-
-            <div class="container mt-5 bg-light pt-5">
-                <div class=" evidenza">
-                    <h2 class="text-center">Our Doctors</h2>
-                    <div class="row row-cols-1 row-cols-md-3 px-5 g-5 pt-5">
-                        <div v-for="doctor in this.doctors" class="col mb-5">
-                            <div class="card">
-                                <img src="../assets/imgs/doc-7.jpg" class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ doctor.name }} {{ doctor.surname }}</h5>
-                                    <p class="card-text">
-                                    <span class="fw-bold" v-for="specialization in doctor.specializations">
-                                        #{{ specialization.title }}
-                                    </span>
-                                    </p>
-                                    <p>{{ doctor.email }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <div class="container mt-5 bg-light pt-5">
+        <div class="evidenza">
+          <h2 class="text-center">Our Doctors</h2>
+          <div class="row row-cols-1 row-cols-md-3 px-5 g-5 pt-5">
+            <div
+              v-for="doctor in this.doctors"
+              class="col mb-5"
+              :key="doctor.id">
+              <div class="card">
+                <img
+                  src="../assets/imgs/doc-7.jpg"
+                  class="card-img-top"
+                  alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">
+                    {{ doctor.name }} {{ doctor.surname }}
+                  </h5>
+                  <p class="card-text">
+                    <span
+                      class="fw-bold"
+                      v-for="specialization in doctor.specializations"
+                      :key="specialization.id">
+                      #{{ specialization.title }}
+                    </span>
+                  </p>
+                  <p>{{ doctor.email }}</p>
                 </div>
+              </div>
             </div>
-
+          </div>
         </div>
-    </main>
-
-
+      </div>
+    </div>
+  </main>
 </template>
 <!-- /template section -->
 
@@ -95,7 +115,7 @@ export default {
 // }
 
 img {
-    max-width: 100%;
+  max-width: 100%;
 }
 
 // .card-img-top {
@@ -103,7 +123,7 @@ img {
 // }
 
 .evidenza {
-    min-height: 200px;
+  min-height: 200px;
 }
 </style>
 <!-- /style section -->
