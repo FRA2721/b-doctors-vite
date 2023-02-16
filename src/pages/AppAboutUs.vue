26724c923745f9d49a76e9f9ed3d08ee67194b9a
luishs0 <luishs.doc@gmail.com>
2023-02-16 14:25:48 +0100

    jumbotron about

--- src/pages/AppAboutUs.vue
+++ src/pages/AppAboutUs.vue
@@ -1,8 +1,8 @@
 <!-- script section -->
 <script>
 export default {
-    // name
-    name: "AppAboutUs",
+  // name
+  name: "AppAboutUs",
 }
 
 </script>
@@ -12,8 +12,13 @@ export default {
 
 <!-- template section -->
 <template>
+  <div class="color-header">
+    <img class="jumbo-about" src="../assets/imgs/jumbo-about1.jpg" alt="">
+    <h2 class="text-center about-title">Siamo lideri in medicina privata</h2>
+  </div>
+  <div class="container">
 
-  <h2>about us</h2>
+  </div>
 
 </template>
 
@@ -22,4 +27,29 @@ export default {
 <!-- style section -->
 <style lang="scss">
 @use '../styles/general.scss' as *;
+
+.jumbo-about {
+  width: 100%;
+  opacity: .6;
+  max-height: 50rem;
+
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+}
+
+.color-header {
+  background-color: #d5eaf2;
+  color: #20254c;
+  position: relative;
+}
+
+.about-title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
