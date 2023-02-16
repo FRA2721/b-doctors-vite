8cb85abf6fd399aa61817ec1ba16dbd59f22a6c6
luishs0 <luishs.doc@gmail.com>
2023-02-16 17:06:48 +0100

    about us

--- src/pages/AppAboutUs.vue
+++ src/pages/AppAboutUs.vue
@@ -16,10 +16,24 @@ export default {
     <img class="jumbo-about" src="../assets/imgs/jumbo-about1.jpg" alt="">
     <h2 class="text-center about-title">Siamo lideri in medicina privata</h2>
   </div>
-  <div class="container">
 
-  </div>
+  <div class="container pt-5 pb-5">
+    <hr>
+    <section class="row ">
+      <div class="section-img col-12 col-md-6 mb-3">
+        <img src="../assets/imgs/world.webp" alt="">
+      </div>
 
+      <div class="col-12 col-md-6 d-flex align-items-center">
+        <h4 class="text-center">Più di 275.000.000 di visite
+          e 2.300.000 appuntamenti ogni anno
+          con i nostri specialisti.
+        </h4>
+      </div>
+    </section>
+    <hr>
+
+</div>
 </template>
 
 
@@ -32,18 +46,15 @@ export default {
   width: 100%;
   opacity: .6;
   max-height: 50rem;
-
-
-  img {
-    width: 100%;
-    object-fit: cover;
-  }
+  object-fit: cover;
+  object-position: top;
 }
 
 .color-header {
   background-color: #d5eaf2;
   color: #20254c;
   position: relative;
+  object-fit: cover;
 }
 
 .about-title {
@@ -51,5 +62,19 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.section-img {
+  text-align: center;
+
+  img {
+    width: 70%;
+  }
+}
+
+.container {
+  hr {
+    border: 2px solid #80c8f5;
+  }
 }
 </style>
