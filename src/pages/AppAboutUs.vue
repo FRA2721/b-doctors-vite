4e45808e164ad64f2d76e6ed4ed4ee5caf4ccc87
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-02-18 23:45:23 +0100

    Fixed ,,About us page"

--- src/pages/AppAboutUs.vue
+++ src/pages/AppAboutUs.vue
@@ -11,28 +11,28 @@ export default {
   <div class="color-header">
     <img class="jumbo-about" src="../assets/imgs/jumbo-about1.jpg" alt="" />
     <div class="about-title container d-none d-md-block">
-      <h1 class="text-start">La Nostra Storia</h1>
+      <h2 class="text-start bold mb-5">Our Story</h2>
       <p>
-        L’Ospedale <bold class="bold">B Doctors</bold> è un ospedale di ricerca
-        e policlinico universitario fondato nel 1971 per fornire cure
-        specializzate per le condizioni di salute più complesse, riconosciuto
-        già nel 1972 come Istituto di Ricovero e Cura a Carattere Scientifico
-        (IRCCS) per la diabetologia. Nel 1991 inizia l’attività di ricerca di
-        base, con l’inaugurazione del dipartimento di biotecnologie (DiBiT),
-        mentre nel 1996 nasce l’Università Vita-Salute San Raffaele. Come
-        conseguenza dell’ampliamento dell’area ricerca, che oggi conta oltre 150
-        laboratori, la qualifica di IRCCS viene estesa nel 2001 a tutto l’ambito
-        della medicina molecolare. Nel 2012 il San Raffaele entra a far parte
-        del Gruppo San Donato (GSD), il più grande gruppo ospedaliero privato in
-        Italia.
+        The <bold class="bold">BD Doctors</bold> Hospital is a research hospital
+        and university general hospital founded in 1971 to provide specialized
+        care for the most complex health conditions, already recognized in 1972
+        as an Institute of Hospitalization and Care of a Scientific Character
+        (IRCCS) for diabetes. Basic research began in 1991, with the
+        inauguration of the biotechnology department (DiBiT), while in 1996 the
+        Vita-Salute San Raffaele University was born. As a consequence of the
+        expansion of the research area, which today has over 150 laboratories,
+        the IRCCS qualification was extended in 2001 to the entire field of
+        molecular medicine. In 2012 the BD Doctors became part of the San Donato
+        Group (GSD), the largest private hospital group in Italy.
       </p>
     </div>
     <div class="about-title container d-md-none text-center title-mobile">
-      Lideri nella medicina provata mondiale.
+      Leaders in world medicine proven.
     </div>
   </div>
 
-  <div class="container pt-5 pb-5">
+  <div class="container color-title pt-5 pb-5">
+    <h2 class="mb-5 bold">Our Numbers</h2>
     <hr />
     <section class="row">
       <div class="section-img col-12 col-md-6 mb-3">
@@ -41,8 +41,9 @@ export default {
 
       <div class="col-12 col-md-6 d-flex align-items-center">
         <h4 class="text-center">
-          Più di 275.000.000 di visite e 2.300.000 appuntamenti ogni anno con i
-          nostri specialisti.
+          More than <bold class="bold">275,000,000</bold> visits and
+          <bold class="bold">2,300,000</bold> appointments every year with our
+          specialists.
         </h4>
       </div>
     </section>
@@ -50,43 +51,65 @@ export default {
   </div>
 
   <div class="container">
-    <div class="row g-3 mb-5">
+    <h2 class="mb-5 bold">Our Strengths</h2>
+    <div class="row g-4 mb-5">
       <div class="col-12 col-md-4">
         <div class="card">
-          <img src="../assets/imgs/img-cure.png" class="card-img-top" alt="...">
+          <img
+            src="../assets/imgs/img-cure.png"
+            class="card-img-top"
+            alt="..." />
           <div class="card-body">
-            <p class="card-text">L’eccellenza medico-scientifica dell’Ospedale San Raffaele nasce
-              dalla triplice identità dell’istituto, un luogo dove ricerca,
-              clinica e formazione universitaria interagiscono quotidianamente.</p>
+            <h3 class="mb-4 mt-5 text-center color-title">
+              State-of-the-Art Care
+            </h3>
+            <p class="card-text text-center">
+              The medical-scientific excellence of the BD Doctors Hospital
+              arises from the triple identity of the institute, a place where
+              research, clinical practice and university education interact on a
+              daily basis.
+            </p>
           </div>
         </div>
       </div>
       <div class="col-12 col-md-4">
         <div class="card">
-          <img src="../assets/imgs/img-alta-tecnologia.png" class="card-img-top" alt="...">
+          <img
+            src="../assets/imgs/img-alta-tecnologia.png"
+            class="card-img-top"
+            alt="..." />
           <div class="card-body">
-            <p class="card-text">Dai robot chirurgici agli strumenti di imaging e radioterapia di
-              precisione: all’Ospedale San Raffaele tecnologie all’avanguardia
-              permettono diagnosi sempre più precoci e cure più personalizzate.</p>
+            <h3 class="mb-4 mt-5 text-center color-title">High Technology</h3>
+            <p class="card-text text-center">
+              From surgical robots to imaging and radiation therapy tools
+              precision: cutting-edge technologies at the BD Doctors Hospital
+              allow ever earlier diagnoses and more personalized treatments.
+            </p>
           </div>
         </div>
       </div>
       <div class="col-12 col-md-4">
         <div class="card">
-          <img src="../assets/imgs/img-appproccio.png" class="card-img-top" alt="...">
+          <img
+            src="../assets/imgs/img-appproccio.png"
+            class="card-img-top"
+            alt="..." />
           <div class="card-body">
-            <p class="card-text">L’eccellenza medico-scientifica dell’Ospedale San Raffaele nasce
-              dalla triplice identità dell’istituto, un luogo dove ricerca,
-              clinica e formazione universitaria interagiscono quotidianamente.</p>
+            <h3 class="mb-4 mt-5 text-center color-title">Multi Approach</h3>
+            <p class="card-text text-center pb-">
+              At the BD Doctors Hospital, diseases are tackled at 360°, thanks
+              to the presence of various specialists who combine their skills at
+              the service of all our patient in the whole world.
+            </p>
           </div>
         </div>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <!-- style section -->
-<style lang="scss">
+<style lang="scss" scoped>
 @use "../styles/general.scss" as *;
 
 .jumbo-about {
@@ -119,7 +142,8 @@ export default {
 }
 
 .bold {
-  font-size: x-large;
+  font-weight: 700;
+  color: #20254c;
 }
 
 .section-img {
@@ -138,5 +162,20 @@ export default {
 
 .title-mobile {
   font-size: 1.6rem;
+}
+
+.color-title {
+  color: #20254c;
+  font-weight: 600;
+}
+
+.card {
+  &:hover {
+    transform: scale(1.04);
+    box-shadow: 10px 5px 5px lightgray;
+  }
+  .card-img-top {
+    height: 10rem;
+  }
 }
 </style>
--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -3,18 +3,13 @@
 export default {
   // name
   name: "AppContact",
-}
-
+};
 </script>
-
-
-
 
 <!-- template section -->
 <template>
   <div class="container">
-
-    <h1 class="text-center mt-3 mb-4">BDoctors Contacts</h1>
+    <h1 class="text-center mt-3 mb-4">BD Doctors Contacts</h1>
 
     <div class="details row text-center text-sm-start align-items-stretch mb-2">
       <div class="title col col-12 col-md-3">
@@ -32,7 +27,6 @@ export default {
       <div class="contact-center col col-12 col-md-3">
         Contact center:
         <p>02 698279829</p>
-
       </div>
     </div>
 
@@ -47,27 +41,24 @@ export default {
       <div class="program col col-12 col-md-3">
         Opening times:
         <p>
-          Lun-Ven 7:00-17:00 <br>
-          Sabato 8:00-12:00 <br>
+          Lun-Ven 7:00-17:00 <br />
+          Sabato 8:00-12:00 <br />
         </p>
       </div>
       <div class="contact-center col col-12 col-md-3">
         Information Admissions Solvency
         <p>
-          Telefono 02 2643 2800 <br>
+          Telefono 02 2643 2800 <br />
           Lun-Ven 7:00-17:00
         </p>
       </div>
     </div>
-
   </div>
 </template>
 
-
-
 <!-- style section -->
 <style lang="scss">
-@use '../styles/general.scss' as *;
+@use "../styles/general.scss" as *;
 
 .col {
   border-left: 1px solid black;
